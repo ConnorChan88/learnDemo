09af91160fe611392e9f09b8879041c9ef4a38cc
ConnorChan <connorchanchuang@gmail.com>
2021-05-11 16:14:37 +0800

    打印

--- src/learnDemo/learn1.java
+++ src/learnDemo/learn1.java
@@ -4,7 +4,9 @@ public class learn1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("测试Demo");
+		int i= 10;
+		float f = 1.23f;
+		String b ="adb";
+		System.out.printf("测试Demo %d",i);
 	}
-
 }
